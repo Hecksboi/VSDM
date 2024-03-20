e7077bd3de2106c77eb14993209b5c81b196ec7b
Zomboi <103043787+Hecksboi@users.noreply.github.com>
2024-03-20 14:30:08 -0700

    Add files via upload

--- VSDM.py
+++ VSDM.py
@@ -1,18 +1,23 @@
 # import random
 import os
 import sys
-import importlib.util
-import re
+#import importlib.util
+from re import split
 from pathlib import Path
 from runpy import run_path
 
 # imports are needed for some file manipulation stuff
 
+desktop_path = Path("/usr/share/")
 home = str(Path.home())
 RUNNING = True
 verbose = False
 found_envs = []
 os.system("touch /tmp/vsdm")
+known_enviornments = []
+x11_desktops = ""
+wayland_desktops = ""
+env_checker_list = []
 
 
 def printred(skk):
@@ -27,24 +32,8 @@ def printyellow(skk):
     print("\033[93m {}\033[00m".format(skk))
 
 
-def printlightpurple(skk):
-    print("\033[94m {}\033[00m".format(skk))
-
-
-def printpurple(skk):
-    print("\033[95m {}\033[00m".format(skk))
-
-
-def printcyan(skk):
-    print("\033[96m {}\033[00m".format(skk))
-
-
-def printlightgray(skk):
-    print("\033[97m {}\033[00m".format(skk))
-
-
-def printblack(skk):
-    print("\033[98m {}\033[00m".format(skk))
+# this is super not how id do it but its compact so i stole it anyway lmao
+# i took this from geeks for geeks
 
 
 def printv(skk):
@@ -54,70 +43,79 @@ def printv(skk):
         pass
 
 
-# this is super not how id do it but its compact so i stole it anyway lmao
-# i took this from geeks for geeks
 try:
     arguments = sys.argv[1]
 except:
     arguments = "-h"
+
 available_commands = [
     [
         "--help, -h",
         "prints info and exits",
     ],
-    #
     [
         "--verbose, -v",
         "prints an excessive amount of extra info for diagonostics",
     ],
-    #
     [
         "--list, -l",
         " prints all available enviornments and exits",
     ],
-    #
     [
         "--list-multi, -m",
         "prints all enviornments with multiple compositors and exits",
     ],
-    #
     [
         "--list-wayland, -w",
         "prints installed wayland enviornments (compositors) and exits",
     ],
-    #
     [
         "--list-x11, -x",
         "prints installed x11 enviornments and exits",
     ],
-    [
-        "--make-configs",
-        "automatically makes the needed files and folders needed for the program",
-    ],
+    # [
+    #     "--make-configs",
+    #     "makes the needed files and folders needed for the program",
+    # ],
 ]
 
 
 def argument_parsing():
     global verbose
     for arg in sys.argv:
+        arg = str(arg)
         if arg == "-h" or arg == "--help" or arguments == "-h":
-            terminal_rows, terminal_columns = os.popen("stty size", "r").read().split()
-            # odd placement because its only needed here
+            term_rows, term_columns = os.popen("stty size", "r").read().split()
             for x in available_commands:
-                description = x[1].rjust(int(terminal_columns) - len(x[0]), " ")
+                description = x[1].rjust(int(term_columns) - len(x[0]), " ")
                 print(f"{x[0]}{description}")
             exit()
-        elif arg == "-v" or arg == "verbose":
-            verbose = True
         elif arg == "-l" or arg == "--list":
+            print("available enviornments:")
             check_available_envs()
-
+            for x in x11_desktops + wayland_desktops:
+                print("    " + x.stem)
+            exit()
+            check_available_envs()
         elif arg == "-m" or arg == "--list-multi":
             print(known_enviornments)
+            exit()
         elif arg == "-x" or arg == "--list-x11":
-            pass
+            print("available x11 enviornments:")
+            check_available_envs()
+            for x in x11_desktops:
+                print("    " + x.stem)
+            exit()
         elif arg == "-w" or arg == "--list-wayland":
-            pass
+            check_available_envs()
+            print("available wayland enviornments:")
+            for x in wayland_desktops:
+                print("    " + x.stem)
+            exit()
+        # elif arg == "--make-configs":
+        #     print("nonfunc")
+        #     exit()
+        #     open(f"")
         else:
             pass
 
@@ -131,7 +129,6 @@ def config_manager():
 
     except:
         try:
-            import fail
 
             printv("checking for conf file in etc conf folder...")
             run_path("/etc/vsdm/config.py")
@@ -142,17 +139,14 @@ def config_manager():
         except:
             printv("both external configs failed, using the local backup...")
             printred(
-                "\nfailed to import config," + "please update user or global config!\n"
+                "\nfailed to import config," "please update user or global config!\n"
             )
             print("using default settings!\n")
 
             user_settings = {
                 "qtile_default": "wayland",
-                "kde_default": "wayland",
                 "plasma_default": "wayland",
-                "kwin_default": "wayland",
                 "gnome_default": "wayland",
-                "mutter_default": "wayland",
                 "enlightenment_default": "x11",
                 "mate_default": "x11",
             }
@@ -161,28 +155,64 @@ def config_manager():
 known_enviornments = [
     # x11 & wayland
     "qtile",
-    "kde",
     "plasma",
     "gnome",
     "enlightenment",
+    "mate",
 ]
 
-
-def find_env():
+def multi_env_checker():
+    global wayland_desktops, x11_desktops
+    for desktop in  wayland_desktops + x11_desktops:
+        for x in known_enviornments:
+            if desktop == x:
+                desktop = ""
+        pass
+    
+def find_chosen_env():
+    x11_options = []
+    wayland_options = []
+    for x in wayland_desktops:
+        wayland_options.append(str(x.stem))
+    for x in x11_desktops:
+        x11_options.append(str(x.stem))
     for x in list(enumerate(known_enviornments)):
         if arguments == x[1]:
-            known_enviornments[x[0]] = True
+            env_checker_list.append(True)
             printv(x[1] + " true!")
+            break
         else:
-            known_enviornments[x[0]] = False
+            env_checker_list.append(False)
             printv(x[1] + " false...")
+            #make this a function
+        if any(env_checker_list):
+            for x in list(enumerate(wayland_options)):
+                if arguments == x[1][0]:
+                    env_checker_list.append(True)
+                    env = 
+                    printv(x[1][0] + " true!")
+                    break
+                else:
+                    env_checker_list.append(False)
+                    printv(x[1][0] + " false...")
+                    #make this a function
+                    
+        if any(env_checker_list):
+            for x in list(enumerate(x11_options)):
+                if arguments == x[1][0]:
+                    env_checker_list.append(True)
+                    printv(x[1][0] + " true!")
+                    break
+                else:
+                    env_checker_list.append(False)
+                    printv(x[1][0] + " false...")
 
 
 def options_checker():
-    if any(known_enviornments):
+    if any(env_checker_list):
         printv("arguments = " + str(sys.argv))
     else:
-        printred("invalid option, format:\n        usdm <enviornment> <opt args>")
+        printred("invalid option, format:" "\n        usdm <enviornment> <opt args>")
         exit()
 
 
@@ -195,44 +225,86 @@ def xinitrc_manager(enviornment):
 
 # print(home) # /home/zomb
 def check_available_envs():
+    global wayland_desktops
+    global x11_desktops
     printv("checking for desktop files...")
-    os.system(
-        "cat $(ls /usr/share/wayland-sessions/*;"
-        " ls /usr/share/xsessions/*) |"
-        " grep --word-regexp Exec > /tmp/vsdm"
-    )
-    # I'm going to try to recreate this with python only,
-    # I was able to before, but it was very bad code and
-    # wrote to disk more than this does.
+    try:
+        printv("    x11...")
+        x11_desktops = list(desktop_path.glob("xsessions/*"))
+        printv("   success!")
+    except:
+        printv(
+            "x11 desktop file search failed, you "
+            "might either have improperly set permissions "
+            "or no xsessions"
+        )
+    try:
+        printv("    wayland...")
+        wayland_desktops = list(desktop_path.glob("wayland-sessions/*"))
+        printv("   success!")
+    except:
+        printv(
+            "wayland desktop file search failed, you "
+            "might either have improperly set permissions "
+            "or no wayland sessions"
+        )
 
-    printv("success!")
+def check_exec():
+    pass
+    # printv("opening .desktop file...") # 
+    # if chosen_env[1] == "wayland":
+    #     with open("/usr/share/", "r") as desktops:
+    #         enviornments = re.split("\n", str(desktops.read()))
+    #         printv("enviornments" + str(enviornments))
+    #         printv("sucess!")
+    #         desktops.close
 
 
-def something():
-    with open("/tmp/vsdm", "r") as desktops:
-        print(desktops.read())
-        desktops.close
+def v_card():
+    global verbose
+    for arg in sys.argv:
+        if arg == "-v" or arg == "--verbose":
+            verbose = True
+
+def env_filter(local_list):
+        if arguments == x[1][0]:
+            env_checker_list.append(True)
+            printv(x[1][0] + " true!")
+        else:
+            env_checker_list.append(False)
+            printv(x[1][0] + " false...")
 
 
 def __main__():
-    something()
+    v_card()
+    check_exec()
     argument_parsing()
     try:
         check_available_envs()
     except:
         printred(
-            "\nchecking for your desktops has failed "
-            "check /usr/share/xsessions for x11 or "
-            "/usr/share/wayland-sessions for wayland"
+            "\nchecking for your desktops has failed"
+            "\ncheck /usr/share/xsessions for x11 or"
+            "\n/usr/share/wayland-sessions for wayland,"
+            "\nif both are there and have .desktop files"
+            "\nplease report issue with -v and the needed"
+            "\nsteps to recreate at"
+            "\n\n    https://github.com/Hecksboi/VSDM/issues"
         )
         exit(1)
-    find_env()
+    find_chosen_env()
     options_checker()
     config_manager()
     try:
         xinitrc_manager(sys.argv[1])
     except:
-        printred("something has gone very wrong!")
+        printred(
+            "something has gone very wrong!"
+            "\nplease report an issue with -v"
+            "and the steps neeeded to recreate at"
+            "\n    https://github.com/Hecksboi/VSDM/issues"
+        )
+        exit(1)
 
 
 __main__()
